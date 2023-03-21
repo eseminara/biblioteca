24dc2dc6f3e160011eaffb5346d9b7debc21eae5
eseminara <eze.seminara@gmail.com>
2023-03-21 15:23:19 -0300

    first commit

--- biblioteca/sett/base.py
+++ biblioteca/sett/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gts9n&%^u&+=8%1*p(m=9@irme07t(zb3)bw#n)fy55sfqgkm0'
+SECRET_KEY = config("SECRET_KEY")
 
 
 # Application definition
